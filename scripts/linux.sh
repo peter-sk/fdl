17c0e25f86f897a0f2660a4cad2b01a682350915
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2023-11-07 14:16:45 +0100

    e Please enter the commit message for your changes. Lines starting

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -2,5 +2,6 @@
 sudo apt-get update
 sudo apt-get install ghc cabal-install
 cabal update
+cabal install --lib base
 cabal install --lib HGL
 ghci -XNPlusKPatterns fdl.hs -e 'runGraphics (withWindow_ "Test" (1000, 600) (\ w -> drawIt (w, 500, 300, 0, 50) [Forward, RightTurn 90, Forward, RightTurn 90, Forward, RightTurn 90, Forward] >> getKey w))'
---
+++ scripts/mac-xquartz.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl https://github.com/XQuartz/XQuartz/releases/download/XQuartz-2.8.5/XQuartz-2.8.5.pkg > ~/XQuartz-2.8.5.pkg
+open ~/XQuartz-2.8.5.pkg
+
