f36715d2065cf0f81389df7549c112e0c719014d
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2023-11-07 14:13:45 +0100

    Prelude requires base package newest cabal version

--- scripts/mac.sh
+++ scripts/mac.sh
@@ -2,5 +2,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install ghc libx11 libxrandr libxscrnsaver cabal-install
 cabal update
+cabal install --lib base
 cabal install --lib HGL
 ghci -XNPlusKPatterns fdl.hs -e 'runGraphics (withWindow_ "Test" (1000, 600) (\ w -> drawIt (w, 500, 300, 0, 50) [Forward, RightTurn 90, Forward, RightTurn 90, Forward, RightTurn 90, Forward] >> getKey w))'
