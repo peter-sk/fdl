f4b5e99ba89054a3dc0bfad347fd52adacd10047
Niels Erik <nijep22@student.sdu.dk>
2023-11-09 11:25:07 +0100

    added another comment to wsl.sh

--- scripts/wsl.sh
+++ scripts/wsl.sh
@@ -7,7 +7,10 @@ sudo apt install libx11-dev libxcomposite-dev libxdamage-dev libxrender-dev libx
 sudo apt install xorg -y
 sudo apt-get install libghc-x11-dev -y
 # maybe only next line instead of lines above
-# sudo apt install libxrandr-dev libxinerama-dev libxss-dev -y 
+# sudo apt install libxrandr-dev libxinerama-dev libxss-dev -y
+
+# atm cabal needs ghc < 9.4, if you a never version do:
+# ghcup install ghc 9.2.5
 sudo apt-get update
 sudo apt-get install ghc cabal-install -y
 cabal update
